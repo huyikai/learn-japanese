cdcebefc89bf7d790d98d851438243f52d681f67
huyikai <hyk_tiger@foxmail.com>
2024-06-21 16:36:01 +0800

    feat: 🎸 new content

--- src/content/config.ts
+++ src/content/config.ts
@@ -3,32 +3,14 @@ import { defineCollection, z } from 'astro:content';
 // 为每一个集合定义一个 `type` 和 `schema`
 const schema = z.object({
   title: z.string(),
-  pubDate: z.date().optional(),
-  description: z.string(),
-  author: z.string(),
-  image: z
-    .object({
-      url: z.string(),
-      alt: z.string()
-    })
-    .optional(),
   tags: z.array(z.string()).optional()
 });
-const postCollection = defineCollection({
+const wordCollection = defineCollection({
   type: 'content',
   schema
 });
-const stackCollection = defineCollection({
-  type: 'content',
-  schema
-});
-const projectCollection = defineCollection({
-  type: 'content',
-  schema
-});
+
 // 导出一个单独的 `collections` 对象来注册你的集合
 export const collections = {
-  post: postCollection,
-  stack: stackCollection,
-  project: projectCollection
+  word: wordCollection
 };
