9e0dd4cfe03b2ed445a73affdf55d1c634e590e5
huyikai <hyk_tiger@foxmail.com>
2024-06-24 09:25:14 +0800

    feat: 🎸 new schema type

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,13 +1,16 @@
 // 从 `astro:content` 导入辅助工具
 import { defineCollection, z } from 'astro:content';
 // 为每一个集合定义一个 `type` 和 `schema`
-const schema = z.object({
+const wordSchema = z.object({
   title: z.string(),
-  tags: z.array(z.string()).optional()
+  tags: z.array(z.string()).optional(),
+  kana: z.string(),
+  tone: z.string(),
+  type: z.string()
 });
 const wordCollection = defineCollection({
   type: 'content',
-  schema
+  schema: wordSchema
 });
 
 // 导出一个单独的 `collections` 对象来注册你的集合
